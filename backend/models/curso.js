b1c51b1ce6cea91331de259fa0dc3e1204c65e09
Ricardo Ramirez <tu_correo@gmail.com>
2026-08-27 11:29:16 -0500

    MUNDO ENGLISH V4

---
+++ backend/models/curso.js
@@ -0,0 +1,28 @@
+
+const mongoose = require("mongoose");
+
+const cursoSchema = new mongoose.Schema({
+
+    titulo: {
+        type: String,
+        required: true
+    },
+
+    descripcion: {
+        type: String,
+        required: true
+    },
+
+    video: {
+        type: String,
+        required: true
+    },
+
+    nivel: {
+        type: String,
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model("curso", cursoSchema);
